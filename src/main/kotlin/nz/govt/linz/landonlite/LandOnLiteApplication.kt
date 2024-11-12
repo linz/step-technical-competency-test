3be1c102a0d28b6c63c3b7994eccb7ef7c149539
Christiano Gontijo <christianogontijo@gmail.com>
2024-11-12 15:45:17 +1300

    BE Changes

--- src/main/kotlin/nz/govt/linz/landonlite/LandOnLiteApplication.kt
+++ src/main/kotlin/nz/govt/linz/landonlite/LandOnLiteApplication.kt
@@ -2,7 +2,7 @@ package nz.govt.linz.landonlite
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
+// BE Changes
 @SpringBootApplication
 class LandOnLiteApplication {
 }
